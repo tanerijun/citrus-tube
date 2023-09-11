9a69b6253c5487828a1346b1c394de9df3528a28
tanerijun <tanerivince@gmail.com>
2023-09-11 16:59:43 +0800

    Auth WIP: logout

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
-import type { LoaderArgs, V2_MetaFunction } from "@remix-run/node"
+import { type ActionArgs, json, type LoaderArgs, type V2_MetaFunction } from "@remix-run/node"
+import { Form, useLoaderData } from "@remix-run/react"
 import { authenticator } from "~/lib/auth/authenticator.server"
 import { db } from "~/lib/db/client.server"
 import { createAccount, getAccount } from "~/lib/services/account.server"
@@ -16,13 +17,22 @@ export const meta: V2_MetaFunction = () => {
 export const loader = async ({ request }: LoaderArgs) => {
 	const user = await authenticator.isAuthenticated(request, { failureRedirect: "/login" })
 
-	return null
+	return json(user)
+}
+
+export const action = async ({ request }: ActionArgs) => {
+	await authenticator.logout(request, { redirectTo: "/" })
 }
 
 export default function Index() {
+	const user = useLoaderData<typeof loader>()
+
 	return (
 		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-			<h1 className="text-red-500 underline">Hello World!</h1>
+			<h1 className="text-red-500 underline">Hello {user.username}!</h1>
+			<Form method="post">
+				<button type="submit">Log out</button>
+			</Form>
 		</div>
 	)
 }
