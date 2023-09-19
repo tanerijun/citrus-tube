a9e4199130492c4b1da95bb67e2a4f739d203cf2
tanerijun <tanerivince@gmail.com>
2023-09-19 18:44:41 +0800

    Update schema

--- app/lib/schema.server.ts
+++ app/lib/schema.server.ts
@@ -1,10 +1,10 @@
 import { sql } from "drizzle-orm"
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
+import { index, integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
 
 export const user = sqliteTable("user", {
 	id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-	username: text("username").notNull(),
-	email: text("email").notNull(),
+	username: text("username").notNull().unique(),
+	email: text("email").notNull().unique(),
 	password: text("password").notNull(),
 	createdAt: integer("created_at", { mode: "timestamp" }).default(sql`(unixepoch('now'))`),
 	profileImageUrl: text("profile_image_url"),
@@ -15,17 +15,25 @@ export const user = sqliteTable("user", {
 export type User = typeof user.$inferSelect
 export type NewUser = typeof user.$inferInsert
 
-export const video = sqliteTable("video", {
-	id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-	title: text("title").notNull(),
-	description: text("description").notNull(),
-	thumbnailUrl: text("thumbnail_url").notNull(),
-	videoUrl: text("video_url").notNull(),
-	userId: integer("user_id")
-		.references(() => user.id, { onDelete: "cascade" })
-		.notNull(),
-	createdAt: integer("created_at", { mode: "timestamp" }).default(sql`(unixepoch('now'))`),
-})
+export const video = sqliteTable(
+	"video",
+	{
+		id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+		title: text("title").notNull(),
+		description: text("description").notNull(),
+		thumbnailUrl: text("thumbnail_url").notNull(),
+		videoUrl: text("video_url").notNull(),
+		userId: integer("user_id")
+			.references(() => user.id, { onDelete: "cascade" })
+			.notNull(),
+		createdAt: integer("created_at", { mode: "timestamp" }).default(sql`(unixepoch('now'))`),
+	},
+	(table) => {
+		return {
+			userIdIdx: index("idx_video_user_id").on(table.userId),
+		}
+	},
+)
 
 export type Video = typeof video.$inferSelect
 export type NewVideo = typeof video.$inferInsert
---
+++ drizzle/migrations/0004_dry_ultron.sql
@@ -0,0 +1,3 @@
+CREATE UNIQUE INDEX `user_username_unique` ON `user` (`username`);--> statement-breakpoint
+CREATE UNIQUE INDEX `user_email_unique` ON `user` (`email`);--> statement-breakpoint
+CREATE INDEX `idx_video_user_id` ON `video` (`user_id`);
