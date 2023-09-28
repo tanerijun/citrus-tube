5c2962f94ce51c96491bfc65827334aba865f6e7
tanerijun <tanerivince@gmail.com>
2023-09-29 00:36:06 +0800

    Enhance register form with Conform

--- app/routes/_auth.register.tsx
+++ app/routes/_auth.register.tsx
@@ -1,58 +1,136 @@
-import { type LoaderFunctionArgs, type ActionFunctionArgs, redirect } from "@remix-run/cloudflare"
-import { Form, Link } from "@remix-run/react"
-import { IconCitrus } from "~/components/icons/citrus"
+import {
+	type LoaderFunctionArgs,
+	type ActionFunctionArgs,
+	redirect,
+	json,
+} from "@remix-run/cloudflare"
+import { z } from "zod"
+import { Form, Link, useActionData } from "@remix-run/react"
 import { Button } from "~/components/ui/button"
 import { Input } from "~/components/ui/input"
 import { Label } from "~/components/ui/label"
 import { getAuth } from "~/lib/auth.server"
+import { parse } from "@conform-to/zod"
+import { useId } from "react"
+import { conform, useForm } from "@conform-to/react"
 
-// TODO: schema. Add confirm password
+const schema = z
+	.object({
+		username: z
+			.string({ required_error: "Username is required" })
+			.min(3, "Username must be at least 3 characters")
+			.max(12, "Username can't be longer than 12 characters"),
+		email: z.string({ required_error: "Email is required" }).email("Invalid email address"),
+		password: z
+			.string({ required_error: "Password is required" })
+			.min(6, "Password should be at least 6 characters")
+			.refine((password) => !password.includes(" "), "Password shouldn't contain whitespaces"),
+		confirmPassword: z.string({ required_error: "Confirm your password" }),
+	})
+	.superRefine(({ password, confirmPassword }, ctx) => {
+		if (password !== confirmPassword) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: "Passwords don't match",
+				path: ["confirmPassword"],
+			})
+		}
+	})
 
 export async function loader({ request, context }: LoaderFunctionArgs) {
 	return await getAuth(context).authenticator.isAuthenticated(request, { successRedirect: "/" })
 }
 
 export async function action({ request, context }: ActionFunctionArgs) {
-	const formData = await request.formData()
-	const username = formData.get("username")
-	const email = formData.get("email")
-	const password = formData.get("password")
+	const formData = await request.clone().formData()
 
-	// TODO: validate
-	if (!username || !email || !password) {
-		throw new Error("Invalid form fields")
+	const submission = parse(formData, { schema })
+
+	if (submission.intent !== "submit" || !submission.value) {
+		console.log("INSIDE IF", submission.value)
+		return json({ submission, message: null })
 	}
 
 	const auth = getAuth(context)
 
-	await auth.register({
-		username: username.toString(),
-		email: email.toString(),
-		password: password.toString(),
-	})
+	const { username, email, password } = submission.value
 
-	return redirect("/login")
+	try {
+		await auth.register({
+			username,
+			email,
+			password,
+		})
+		return redirect("/login")
+	} catch (error) {
+		if (error instanceof Error) {
+			return json({ submission, message: error.message })
+		}
+		return json({ submission, message: "Unexpected error" })
+	}
 }
 
 export default function Register() {
+	const data = useActionData<typeof action>()
+
+	const msg = data?.message // TODO: show as notification
+
+	console.log(msg) //TODO: DELETE
+
+	const id = useId()
+
+	const [form, fields] = useForm({
+		id,
+		lastSubmission: data?.submission,
+		shouldValidate: "onBlur",
+		onValidate({ formData }) {
+			return parse(formData, { schema })
+		},
+	})
+
 	return (
 		<main className="mx-6 flex h-screen flex-col items-center justify-center gap-6">
 			<h2 className="text-2xl">Welcome to Citrus!</h2>
 			<Form
 				method="post"
 				className="border-input flex w-full flex-col gap-6 rounded-md border p-10 md:w-96"
+				{...form.props}
 			>
 				<div className="flex flex-col gap-2">
-					<Label htmlFor="username">Username</Label>
-					<Input id="username" type="text" name="username" required />
+					<Label htmlFor={fields.username.id}>Username</Label>
+					<Input {...conform.input(fields.username, { type: "text" })} />
+					{fields.username.error && (
+						<p id={fields.username.errorId} className="text-destructive text-sm">
+							{fields.username.error}
+						</p>
+					)}
 				</div>
 				<div className="flex flex-col gap-2">
-					<Label htmlFor="email">Email</Label>
-					<Input id="email" type="email" name="email" required />
+					<Label htmlFor={fields.email.id}>Email</Label>
+					<Input {...conform.input(fields.email, { type: "email" })} />
+					{fields.email.error && (
+						<p id={fields.email.errorId} className="text-destructive text-sm">
+							{fields.email.error}
+						</p>
+					)}
 				</div>
 				<div className="flex flex-col gap-2">
-					<Label htmlFor="password">Password</Label>
-					<Input type="password" name="password" required />
+					<Label htmlFor={fields.password.id}>Password</Label>
+					<Input {...conform.input(fields.password, { type: "password" })} />
+					{fields.password.error && (
+						<p id={fields.password.errorId} className="text-destructive text-sm">
+							{fields.password.error}
+						</p>
+					)}
+				</div>
+				<div className="flex flex-col gap-2">
+					<Label htmlFor={fields.confirmPassword.id}>Confirm password</Label>
+					<Input {...conform.input(fields.confirmPassword, { type: "password" })} />
+					{fields.confirmPassword.error && (
+						<p id={fields.confirmPassword.errorId} className="text-destructive text-sm">
+							{fields.confirmPassword.error}
+						</p>
+					)}
 				</div>
 				<Button type="submit">Register</Button>
 				<small>
