2d7763d424c17fcafa796b52db058fc558764bd8
tanerijun <tanerivince@gmail.com>
2023-10-14 13:30:52 +0800

    Send only necessary owner data with video

--- app/lib/services/video.server.ts
+++ app/lib/services/video.server.ts
@@ -3,7 +3,12 @@ import { getDb } from "../db.server"
 export async function getVideos(count = 20) {
 	const db = getDb()
 
-	const results = await db.query.video.findMany({ with: { owner: true }, limit: count })
+	const results = (await db.query.video.findMany({ with: { owner: true }, limit: count })).map(
+		({ owner, ...rest }) => ({
+			owner: { id: owner.id, username: owner.username, profileImageUrl: owner.profileImageUrl },
+			...rest,
+		}),
+	)
 
 	return results
 }
