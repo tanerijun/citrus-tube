60f06204519783b2bef5aa7235d81761f189218c
tanerijun <tanerivince@gmail.com>
2023-10-21 13:51:01 +0800

    Make video grid responsive

--- app/routes/_home._index.tsx
+++ app/routes/_home._index.tsx
@@ -36,7 +36,7 @@ export default function Index() {
 	const videos = useLoaderData<typeof loader>()
 
 	return (
-		<div className="grid grid-cols-3 gap-4">
+		<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
 			{videos.map((video) => {
 				return (
 					<Card key={video.id} className="relative h-full">
