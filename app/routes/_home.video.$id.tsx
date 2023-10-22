792df4326f13adf712c118873f852c4f2696da76
tanerijun <tanerivince@gmail.com>
2023-10-22 09:39:40 +0800

    Fetch video data

--- app/routes/_home.video.$id.tsx
+++ app/routes/_home.video.$id.tsx
@@ -1,10 +1,28 @@
-import type { LoaderFunctionArgs } from "@remix-run/cloudflare"
+import { json, type LoaderFunctionArgs } from "@remix-run/cloudflare"
+import { useLoaderData } from "@remix-run/react"
+import { getVideoById } from "~/server/services/video.server"
 
-export const loader = ({ params }: LoaderFunctionArgs) => {
-	console.log(params.id)
-	return null
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+	const param = params.id
+	if (!param) {
+		throw new Error("Missing param: id")
+	}
+
+	const id = parseInt(param)
+	if (isNaN(id)) {
+		throw new Response("Not Found", { status: 404 })
+	}
+
+	const video = await getVideoById(id)
+	if (!video) {
+		throw new Response("Not Found", { status: 404 })
+	}
+
+	return json(video)
 }
 
 export default function VideoPage() {
-	return <h1>Video</h1>
+	const video = useLoaderData<typeof loader>()
+
+	return <h1>{video.title}</h1>
 }
--- app/server/services/video.server.ts
+++ app/server/services/video.server.ts
@@ -12,3 +12,23 @@ export async function getVideos(count = 20) {
 
 	return results
 }
+
+export async function getVideoById(id: number) {
+	const db = getDb()
+
+	const result = await db.query.video.findFirst({
+		with: { owner: true },
+		where: (video, { eq }) => eq(video.id, id),
+	})
+
+	if (!result) {
+		return null
+	}
+
+	const { owner } = result
+
+	return {
+		...result,
+		owner: { id: owner.id, username: owner.username, profileImageUrl: owner.profileImageUrl },
+	}
+}
