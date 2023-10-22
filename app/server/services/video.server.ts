ee7ea286e84964bbe2454068c3ef641658222cad
tanerijun <tanerivince@gmail.com>
2023-10-22 12:11:54 +0800

    Refactor video service and add function to find by pattern

--- app/server/services/video.server.ts
+++ app/server/services/video.server.ts
@@ -3,32 +3,26 @@ import { getDb } from "../db.server"
 export async function getVideos(count = 20) {
 	const db = getDb()
 
-	const results = (await db.query.video.findMany({ with: { owner: true }, limit: count })).map(
-		({ owner, ...rest }) => ({
-			owner: { id: owner.id, username: owner.username, profileImageUrl: owner.profileImageUrl },
-			...rest,
-		}),
-	)
-
-	return results
+	return await db.query.video.findMany({
+		with: { owner: { columns: { id: true, username: true, profileImageUrl: true } } },
+		limit: count,
+	})
 }
 
 export async function getVideoById(id: number) {
 	const db = getDb()
 
-	const result = await db.query.video.findFirst({
-		with: { owner: true },
+	return await db.query.video.findFirst({
+		with: { owner: { columns: { id: true, username: true, profileImageUrl: true } } },
 		where: (video, { eq }) => eq(video.id, id),
 	})
+}
 
-	if (!result) {
-		return null
-	}
+export async function getVideosLike(pattern: string) {
+	const db = getDb()
 
-	const { owner } = result
-
-	return {
-		...result,
-		owner: { id: owner.id, username: owner.username, profileImageUrl: owner.profileImageUrl },
-	}
+	return await db.query.video.findMany({
+		with: { owner: { columns: { id: true, username: true, profileImageUrl: true } } },
+		where: (video, { like }) => like(video.title, pattern),
+	})
 }
